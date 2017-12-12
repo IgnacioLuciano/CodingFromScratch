91eee423060584aa7fdef870e3b92bc4bc293bd0
Unknown <iglumu@gmail.com>
2017-12-11 22:06:37 -0800

    Stuck in Ex9

---
+++ script.js
@@ -0,0 +1,19 @@
+var myFavGotChar = "Shae Snow",
+
+function checkMyFavChar(value1="Shae Snow"){
+    
+if (value1 = "Tyrion Lannister"){
+  alert ("Wrong answer, try again!");
+}
+  else if (value1 = "Jon Snow"){
+  alert ("Incorrect answer, try one more time")
+}
+  else (value1==any);{
+  alert ("The correct answer is 'Shae Snow'")
+  } 
+}
+
+function checkMyFavChar(a, b) {
+    return ("The correct answer is 'Shae Snow'")
+}
+        
